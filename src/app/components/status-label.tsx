f09d0af5148974d560b5d2d368666cfcef38a9dc
YagovkinDmytro <139924381+YagovkinDmytro@users.noreply.github.com>
2026-03-20 17:25:36 +0100

    Add status labels

---
+++ src/app/components/status-label.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import clsx from 'clsx';
+
+export enum Status {
+  Active = 'active',
+  NotActive = 'notActive',
+  Pending = 'pending',
+  Suspended = 'suspended',
+}
+
+export interface StatusLabelProps {
+  children: React.ReactNode;
+  status: Status;
+  disabled?: boolean;
+}
+
+export default function StatusLabel({
+  children,
+  status,
+  disabled,
+}: StatusLabelProps) {
+  return (
+    <span
+      className={clsx(
+        'inline-flex items-center py-1 px-3.5 rounded-3xl text-sm font-medium',
+        status === Status.Active && 'text-[#15803d] bg-[#dcfce7]',
+        status === Status.NotActive && 'text-[#b91c1c] bg-[#fee2e2]',
+        status === Status.Pending && 'text-[#c2410c] bg-[#ffedd5]',
+        status === Status.Suspended && 'text-[#1d4ed8] bg-[#dbeafe]',
+        {
+          ['opacity-75 cursor-not-allowed']: disabled,
+        },
+      )}
+    >
+      <div className='w-1 h-1 mr-2 rounded-full bg-current' />
+      {children}
+    </span>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
+import StatusLabel, { Status } from './components/status-label';
+
 export default function Home() {
   return (
-    <div className='flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
-      <main className='flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start'>
-        <h1>Home Page</h1>
+    <div className=''>
+      <main className=''>
+        <h1 className='text-xl'>Home Page</h1>
+        <StatusLabel status={Status.Active}>Active</StatusLabel>
+        <StatusLabel status={Status.NotActive} disabled>
+          Not Active
+        </StatusLabel>
+        <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+        <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
       </main>
     </div>
   );
