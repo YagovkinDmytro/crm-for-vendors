7121c12f73c85652a027c1c058285a1303917000
YagovkinDmytro <139924381+YagovkinDmytro@users.noreply.github.com>
2026-04-27 21:40:17 +0200

    Add using fetch

--- src/app/(admin)/companies/@header/[id]/page.tsx
+++ src/app/(admin)/companies/@header/[id]/page.tsx
@@ -4,6 +4,7 @@ export interface PageProps {
   params: { id: string };
 }
 
-export default function Page({ params }: PageProps) {
-  return <Header>{`Company(${params.id})`}</Header>;
+export default async function Page({ params }: PageProps) {
+  const { id } = await params;
+  return <Header>{`Company(${id})`}</Header>;
 }
--- src/app/(admin)/companies/[id]/page.tsx
+++ src/app/(admin)/companies/[id]/page.tsx
@@ -1,20 +1,13 @@
-import { notFound } from 'next/navigation';
-
 export interface PageProps {
   params: { id: string };
 }
 
 export default async function Page({ params }: PageProps) {
   const { id } = await params;
-  const numericId = Number.parseInt(id, 10);
-
-  if (Number.isNaN(numericId)) {
-    notFound();
-  }
 
   return (
     <div className='py-6 px-10'>
-      <p>{`Information about company (${params.id})`}</p>
+      <p>{`Information about company (${id})`}</p>
     </div>
   );
 }
